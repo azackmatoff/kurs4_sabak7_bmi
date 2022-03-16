6eb4ce68a9938666d1a8469b8f7911d150eeea36
AZ Ackmatoff <ackmatoff@gmail.com>
2022-03-16 20:38:24 +0300

    added const

--- lib/app_constants/colors/app_colors.dart
+++ lib/app_constants/colors/app_colors.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color mainColor = Color(0xff0b0e21);
-  static Color secondaryColor = Color(0xff24263B);
-  static Color white = Color(0xffffffff);
-  static Color black = Color(0xff000000);
-  static Color red = Colors.red;
-  static Color redDark = Color(0x29EB1555);
-  static Color redAccent = Colors.redAccent;
-  static Color selected = Color(0xff24263B);
-  static Color unselected = Color(0xFF181B2C);
-  static Color greyDarkish = Color(0xFF4D516B);
-  static Color greyDark = Color(0xff323244);
-  static Color greenLight = Color(0xFF43DA8C);
+  static const Color mainColor = Color(0xff0b0e21);
+  static const Color secondaryColor = Color(0xff24263B);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+  static const Color red = Colors.red;
+  static const Color redDark = Color(0x29EB1555);
+  static const Color redAccent = Colors.redAccent;
+  static const Color selected = Color(0xff24263B);
+  static const Color unselected = Color(0xFF181B2C);
+  static const Color greyDarkish = Color(0xFF4D516B);
+  static const Color greyDark = Color(0xff323244);
+  static const Color greenLight = Color(0xFF43DA8C);
 }
--- lib/app_constants/text_styles/app_text_styles.dart
+++ lib/app_constants/text_styles/app_text_styles.dart
@@ -2,25 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:kurs4_sabak7_bmi/app_constants/colors/app_colors.dart';
 
 class AppTextStyles {
-  static TextStyle mainButton = TextStyle(
+  static const TextStyle mainButton = TextStyle(
     color: AppColors.white,
     fontSize: 25,
   );
-  static TextStyle ageOrWeight = TextStyle(
+  static const TextStyle ageOrWeight = TextStyle(
     color: AppColors.white,
     fontSize: 60,
   );
-  static TextStyle circleButton = TextStyle(
+  static const TextStyle circleButton = TextStyle(
     color: AppColors.white,
     fontSize: 42,
     fontWeight: FontWeight.bold,
   );
-  static TextStyle resultSubject = TextStyle(
+  static const TextStyle resultSubject = TextStyle(
     color: AppColors.greenLight,
     fontSize: 20,
     fontWeight: FontWeight.bold,
   );
-  static TextStyle resultBigText = TextStyle(
+  static const TextStyle resultBigText = TextStyle(
     fontSize: 100,
     fontWeight: FontWeight.bold,
   );
